14bd0d51d7f64e8c7578591853d475f2a476ab89
balaguru-20 <balaguravaiah0606@gmail.com>
2025-04-30 00:20:17 +0530

    terraform test module for vpc

--- variables.tf
+++ variables.tf
@@ -1,25 +1,25 @@
 variable "project_name" {
-    default = "expense" 
+  default = "expense"
 }
 
 variable "environment" {
-    default = "dev"  
+  default = "dev"
 }
 
 variable "cidr_block" {
-    default = "10.0.0.0/16"  
+  default = "10.0.0.0/16"
 }
 
 variable "common_tags" {
-    default = {
-        Project = "expense"
-        Environment = "dev"
-        Terraform = "true"
-    } 
+  default = {
+    Project     = "expense"
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
 
 variable "vpc_tags" {
-    default = {
-        Purpose = "assignment"
-    }  
+  default = {
+    Purpose = "assignment"
+  }
 }
